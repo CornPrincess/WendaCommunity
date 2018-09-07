e0c8eeb1a0d0b3e90477e9aeaf8484884e3faba5
ztb1211 <xiaocorn96@gmail.com>
2018-09-07 22:44:35 +0800

    define HostHolder component

--- src/main/java/com/minmin/wenda/model/HostHolder.java
+++ src/main/java/com/minmin/wenda/model/HostHolder.java
@@ -1,8 +1,27 @@
 package com.minmin.wenda.model;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author corn
  * @version 1.0.0
  */
+
+@Component
 public class HostHolder {
+    // TODO 学习什么是ThreadLocal
+    // 线程本地变量，底层可以理解为Map<Thread, user>
+    private static ThreadLocal<User> users = new ThreadLocal<>();
+
+    public User getUser(){
+        return users.get();
+    }
+
+    public void setUser(User user) {
+        users.set(user);
+    }
+
+    public void clear() {
+
+    }
 }
